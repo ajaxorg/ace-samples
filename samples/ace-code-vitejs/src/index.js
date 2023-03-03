c945fdfb6a17e2795363fa87ffec5d9592db4e2b
nightwing <amirjanyan@gmail.com>
2023-03-03 16:31:13 +0400

    add cdn sample

---
+++ samples/ace-code-vitejs/src/index.js
@@ -0,0 +1,20 @@
+import ace from 'ace-builds';
+import 'ace-builds/esm-resolver';
+import "./chrome.css.js"
+//^ you could use direct imports instead like
+//import 'ace-builds/src-noconflict/mode-html'; for HTML mode
+//or import "ace-builds/src-noconflict/theme-eclipse.js"; for theme
+
+import {LanguageProvider} from "ace-linters";
+
+let worker = new Worker(new URL('./webworker.js', import.meta.url), {type: "module"});
+let languageProvider = LanguageProvider.create(worker);
+
+let editor = ace.edit("container", {
+    useWorker: false
+});
+editor.session.setValue("<html>\n    <h1>Hello world!</h1>\n</html>");
+editor.setTheme("ace/theme/eclipse");
+editor.session.setMode("ace/mode/html");
+
+languageProvider.registerEditor(editor);
